74a6961498c08f86bfbadb839f84ef0473103710
crocket77 <ethanpfeiffer345@outlook.com>
2022-05-08 00:39:35 -0500

    gallery photos now open as a modal once clicked on

--- src/components/Photolist/index.js
+++ src/components/Photolist/index.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react'
+import Modal from '../Modal';
 
 
 
 const PhotoList = ({ category }) =>{
+  const [currentPhoto, setCurrentPhoto] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [photos] = useState([
     {
       name: 'Grocery aisle',
@@ -100,15 +103,24 @@ const PhotoList = ({ category }) =>{
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     },
   ]);
+
   const currentPhotos = photos.filter((photo) => photo.category === category);
+
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({...image, index: i});
+    setIsModalOpen(!isModalOpen);
+  }
+
   return (
     <div>
+    {isModalOpen && <Modal currentPhoto={currentPhoto} onClose={toggleModal}/>}
     <div className="flex-row">
     {currentPhotos.map((image, i) => (
           <img
             src={require(`../../assets/small/${category}/${i}.jpg`)}
             alt={image.name}
             className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
